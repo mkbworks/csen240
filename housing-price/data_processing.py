87dc873cee85e701e365c5bb98437681d8a24916
Mahesh Kumaar Balaji <maheshkumaar.balaji@outlook.com>
2025-01-21 22:47:54 -0800

    Chore: Added descriptive comments for DataProcessing class

--- housing-price/data_processing.py
+++ housing-price/data_processing.py
@@ -2,7 +2,14 @@ import pandas as pd
 import math
 
 class DataProcessing:
+    """
+    Class to fetch data from .csv file, clean and transform them into the desired format.
+    """
     def __init__(self, file):
+        """
+        Constructor to create a new instance of 'DataProcessing'.
+        :param file: Complete file path of the .csv file containing the dataset.
+        """
         self.file = str(file).strip()
         df = pd.read_csv(self.file, parse_dates=True)
         total_rows = df.shape[0]
@@ -11,7 +18,10 @@ class DataProcessing:
         self.validation_data = df[training_rows:]
     
     def clean_data(self):
-        # Clean price and square footage values for training data
+        """
+        Function to clean the training and validation data to remove unwanted columns and change values to a format suitable for training the machine learning model.
+        :returns: no value(s).
+        """
         self.training_data['Price'] = self.training_data['Price'] / 10000
         self.training_data['Square_Footage'] = self.training_data['Square_Footage'] / 100
 
@@ -20,6 +30,10 @@ class DataProcessing:
         self.validation_data['Square_Footage'] = self.validation_data['Square_Footage'] / 100
     
     def get_training_data(self):
+        """
+        Function that returns the feature and target values for training the machine learning model.
+        :returns: tuple (ndarray object of feature values, ndarray object of target values)
+        """
         self.clean_data()
         xi = self.training_data['Square_Footage'].to_numpy(dtype='float')
         yi = self.training_data['Price'].to_numpy(dtype='float')
