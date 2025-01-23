7562f9f4e8cf0c21d7320faeed8f46c1356b770b
Mahesh Kumaar Balaji <maheshkumaar.balaji@outlook.com>
2025-01-22 17:32:09 -0800

    Changed the dataset for price estimation and added notebook for dataset analysis

--- housing-price/data_processing.py
+++ housing-price/data_processing.py
@@ -5,13 +5,11 @@ class DataProcessing:
     """
     Class to fetch data from .csv file, clean and transform them into the desired format.
     """
-    def __init__(self, file):
+    def __init__(self):
         """
-        Constructor to create a new instance of 'DataProcessing'.
-        :param file: Complete file path of the .csv file containing the dataset.
+        Constructor to create a new instance of 'DataProcessing'. The first 80% of rows are allotted as training data and the rest 20% are allotted as validation data.
         """
-        self.file = str(file).strip()
-        df = pd.read_csv(self.file, parse_dates=True)
+        df = pd.read_csv("housing.csv", parse_dates=True)
         total_rows = df.shape[0]
         training_rows = math.ceil(0.8 * total_rows)
         self.training_data = df[:training_rows]
@@ -22,12 +20,12 @@ class DataProcessing:
         Function to clean the training and validation data to remove unwanted columns and change values to a format suitable for training the machine learning model.
         :returns: no value(s).
         """
-        self.training_data['Price'] = self.training_data['Price'] / 10000
-        self.training_data['Square_Footage'] = self.training_data['Square_Footage'] / 100
+        self.training_data['area'] = self.training_data['area'] / 1000
+        self.training_data['price'] = self.training_data['price'] / 1000000
 
         # Clean price and square footage values for validation data
-        self.validation_data['Price'] = self.validation_data['Price'] / 10000
-        self.validation_data['Square_Footage'] = self.validation_data['Square_Footage'] / 100
+        self.validation_data['area'] = self.validation_data['area'] / 1000
+        self.validation_data['price'] = self.validation_data['price'] / 1000000
     
     def get_training_data(self):
         """
@@ -35,6 +33,6 @@ class DataProcessing:
         :returns: tuple (ndarray object of feature values, ndarray object of target values)
         """
         self.clean_data()
-        xi = self.training_data['Square_Footage'].to_numpy(dtype='float')
-        yi = self.training_data['Price'].to_numpy(dtype='float')
+        xi = self.training_data['area'].to_numpy(dtype='float')
+        yi = self.training_data['price'].to_numpy(dtype='float')
         return xi, yi
